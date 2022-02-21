e8cf48c8c1fde71bfc4497057026cce3b399b1a4
mianuar <53685685+imananuar@users.noreply.github.com>
2022-02-21 18:15:49 +0800

    Added Image and header copywriting

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import './Circular.css';
 import Navbar from './components/Navbar';
 import SecondHeader from './components/SecondHeader';
+import ThirdHeader from './components/ThirdHeader';
 
 function App() {
   return (
     <header className="">
       <Navbar />
       <SecondHeader />
+      <ThirdHeader />
     </header>
   );
 }
---
+++ src/components/ThirdHeader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ThirdHeader.css'
+
+function ThirdHeader() {
+  return (
+    <div className='wrapper'>
+        <div className='header-image '>
+            <div className="text-wrapper">
+                <h1 class="header-copywriting">Not sure where to go? Perfect.</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ThirdHeader
