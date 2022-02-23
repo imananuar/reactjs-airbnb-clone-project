1081ca50ac83ca067e8a872841f0bfb111fe7c59
mianuar <53685685+imananuar@users.noreply.github.com>
2022-02-23 22:31:03 +0800

    Question section added

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './Question.css';
 
 function Question() {
   return (
-    <div>Question</div>
+    <section className="hosting">
+      <div className='hosting-wrapper'>
+        <div className='hosting-child-components'>
+          <div className='hosting-container'>
+            <div className='question'>Questions about hosting?</div>
+          </div>
+          <div className='superhost-button'>
+            <button className="ask-button">Ask a Superhost</button>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
