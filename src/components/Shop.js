7940481a7feaa3d76683dc46cdca381613894c5c
mianuar <53685685+imananuar@users.noreply.github.com>
2022-02-23 21:51:00 +0800

    shop section

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './Shop.css';
+import airbnbCard from "./images/airbnb-card.webp"
 
 function Shop() {
   return (
-    <div>Shop</div>
+    <section className="shop">
+      <div className='shop-container'>
+        <div className='shop-content'>
+          <div className='shop-headline'>
+            Shop Airbnb gift cards
+          </div>
+          <div className='learn-more'>
+            <button className='learn-more-button'>Learn More</button>
+          </div>
+        </div>
+        <div className='shop-img-div'>
+          <img src={airbnbCard} className='shop-img'/>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
